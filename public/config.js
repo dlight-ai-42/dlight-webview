3439ac754f3cc4b9b74fa82a5f9cce3a01804471
doris <doris070714@gmail.com>
2026-05-22 17:59:23 +0800

    mv folder

---
+++ public/config.js
@@ -0,0 +1,6 @@
+const DLIGHT_CONFIG = {
+  SUPABASE_URL: 'https://vfyaozbbopqampohrnpa.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmeWFvemJib3BxYW1wb2hybnBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2Nzg2MTgsImV4cCI6MjA5NDI1NDYxOH0.ClkN18Qk2nx-zLb1omWrbM85puUhuDU_SsrOEuAfRsg',
+  ELDER_NAME: '爸爸',
+  REFRESH_INTERVAL_MS: 60000
+};
